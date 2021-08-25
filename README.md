3fedc46f4dba9669d97f3b9904f73848de6f222a
MartinMorici <martinmorici3@gmail.com>
2021-08-25 19:55:05 -0300

    Incluyo cambios curso JavaScript

---
+++ README.md
@@ -0,0 +1,2 @@
+Proyecto para curso de desarrollo web de CoderHouse. <br/>
+[Link a la web](https://martinmorici.github.io/DeLaCasa/)
---
+++ js/mates.js
@@ -0,0 +1,9 @@
+setTimeout(()=>{
+    let mates = productos.filter(p => p.tipo === "mate");
+    interfazProductos(mates)
+    // filtrado.onclick = filtrarPorPrecio(termos);
+    let filtrado = document.getElementById("filtro1");
+    filtrado.addEventListener("click", function () {
+      filtrarPorPrecio(mates);
+    });
+},100)
